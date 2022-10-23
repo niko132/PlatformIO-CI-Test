5be47b7b80ae677ea0b169dbc065b00c33cb3b00
Niko Kirste <nikokirste@hotmail.de>
2022-10-23 21:08:48 +0200

    some changes

--- test/test_dithering.cpp
+++ test/test_dithering.cpp
@@ -15,9 +15,14 @@ void test_dither_is_initialized() {
     TEST_ASSERT_TRUE(_updatesPerFrame == 8);
 }
 
+void some_useless_test() {
+    TEST_ASSERT_TRUE(true);
+}
+
 void setup() {
     ESP_TEST_BEGIN();
     RUN_TEST(test_dither_is_initialized);
+    RUN_TEST(some_useless_test);
     ESP_TEST_END();
 }
 
